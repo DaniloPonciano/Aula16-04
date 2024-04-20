b6a2e10f3325896c0fcc079665d1e99121c07702
danilo_ponciano <danilo_ponciano@estudante.sc.senai.br>
2024-04-19 21:42:17 -0300

    Criando Tabela html para registro de convidados e de acompanhante

--- src/main/java/com/convidados/controller/ConvidadosController.java
+++ src/main/java/com/convidados/controller/ConvidadosController.java
@@ -5,6 +5,7 @@ import com.convidados.repository.ConvidadosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -19,6 +20,12 @@ public class ConvidadosController {
         mv.addObject("Convidados", convidadosRepository.findAll());
 
         return mv;
+    }
+
+    @PostMapping("/convidados")
+    public String salvar(Convidados convidados){
+        this.convidadosRepository.save(convidados);
+
     }
 
 }
